36ea79676d1350f831adf96047bd7b4c7759fc85
Ali Abdallah <aliabdallah93@gmail.com>
2018-08-08 07:38:58 +0200

    use bootstrap, create page header component and use tajawal logo

--- tajawal/src/App.js
+++ tajawal/src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Jumbotron, Button } from 'react-bootstrap';
+import PageHeader from './shared/components/header/PageHeader';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <PageHeader />
+        <Jumbotron>
+          <h1>Hello, world!</h1>
+          <p>
+            This is a simple hero unit, a simple jumbotron-style component for calling
+            extra attention to featured content or information.
+          </p>
+          <p>
+            <Button bsStyle="primary">Learn more</Button>
+          </p>
+        </Jumbotron>
       </div>
     );
   }
---
+++ tajawal/src/shared/components/header/PageHeader.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { PageHeader as BootstrapPageHeader } from 'react-bootstrap';
+import './PageHeader.css';
+
+class PageHeader extends Component {
+    render() {
+        return (
+            <BootstrapPageHeader className="PageHeader">
+                <img src="https://tjwlcdn.com/cms/assets/logo/logo-rebrand-ltr.svg" className="PageHeader-logo container img-responsive" alt="tajawal" />
+            </BootstrapPageHeader>
+        );
+    }
+}
+
+export default PageHeader;
