c65d94f89d8fd278ba31d66a5062dd4bf5f92035
Ali Abdallah <aliabdallah93@gmail.com>
2018-08-08 21:08:26 +0200

    add search button and functionality

--- tajawal/src/components/Home/Home.js
+++ tajawal/src/components/Home/Home.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Jumbotron } from 'react-bootstrap';
+import { Jumbotron, Button } from 'react-bootstrap';
 import './Home.css';
 import DatePicker from 'react-date-picker';
+// import DatePicker from 'react-date-picker/dist/entry.nostyle';
 
 class Home extends Component {
     state = {
@@ -13,12 +14,18 @@ class Home extends Component {
 
     onEndDateChange = endDate => this.setState({ endDate })
 
+    search = () => {
+        console.log()
+    }
+
     render() {
         return (
             <Jumbotron>
-                <h1>Search around the world</h1>
-                <DatePicker onChange={this.onStartDateChange} value={this.state.startDate} />
-                <DatePicker onChange={this.onEndDateChange} value={this.state.endDate} />
+                <h1>Search for hotels all around the world</h1>
+                <DatePicker className="Home-date-picker" clearIcon={null} required={true} onChange={this.onStartDateChange} value={this.state.startDate} />
+                <DatePicker className="Home-date-picker" clearIcon={null} required={true} onChange={this.onEndDateChange} value={this.state.endDate} />
+                <br />
+                <Button bsStyle="danger" onClick={this.search}>Search</Button>
             </Jumbotron>
         );
     }
